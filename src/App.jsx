92b3f13b988870a400e9c70c245e6583d4f2141d
Leoander Neves <leoanderneves8@gmail.com>
2024-08-16 00:06:44 +0000

    create card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h1>Java</h1>
-        <img src="https://salvatore.academy/devmon/1_java.png" alt="DevMon Java" width="200" />
-      </div>
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import './Card.css';
+
+export default function Card() {
+  return (
+    <div>
+      <div className="card">
+        <h1>Java</h1>
+        <img
+          src="https://salvatore.academy/devmon/1_java.png"
+          alt="DevMon Java"
+          width="200"
+        />
+      </div>
+    </div>
+  );
+}
